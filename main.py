597f27a7f6c337e8f02438beec1f89765b0b5586
Eliaz Bobadilla <71897736+UltiRequiem@users.noreply.github.com>
2021-03-04 20:37:08 -0500

    Fixing Bugs

--- main.py
+++ main.py
@@ -1,102 +1,61 @@
 import os
+import discord
 from keep_alive import keep_alive
 from discord.ext import commands
-import discord
 
-client = commands.Bot(command_prefix='uwu ') 
-@client.command()
+bot = commands.Bot(
+	command_prefix="yuno ", 
+	case_insensitive=True  
+)
+
+bot.author_id = 413720448627638274 # Change to your discord id!!!
+
+#-----------Text Commands----------------
+
+@bot.command()
 async def ping(ctx):
-    await ctx.send(f"Pong!\n{round(client.latency * 1000)}ms") 
+    await ctx.send(f"Pong!\n{round(bot.latency * 1000)}ms") 
 
+@bot.command()
+async def owo(owo_mesagge):
+    await owo_mesagge.send('O//w//O')
 
-@client.event
-async def on_ready():
-    game = discord.Game('Killing Furrys')
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Zero Requiem"))
+#------------Utilities---------------------
 
-
-@client.command()
-async def ping(ctx):
-    await ctx.send(f"Pong!\n{round(client.latency * 1000)}ms")
-
-
-@client.command()
-async def owo(ctx):
-    await ctx.send('O//w//O')
-
-
-@client.command()
+@bot.command()
 async def sum(ctx, num_one: int, num_two: int):
     await ctx.send(num_one + num_two)
 
+@bot.command()
+async def res(ctx, num_one: int, num_two: int):
+    await ctx.send(num_one - num_two)
 
-@client.command()
-async def info(ctx):
-    name = str(ctx.guild.name)
-    description = str(ctx.guild.description)
-    owner = str(ctx.guild.owner)
-    id = str(ctx.guild.id)
-    region = str(ctx.guild.region)
-    member_count = str(ctx.guild.member_count)
+@bot.command()
+async def mul(ctx, num_one: int, num_two: int):
+    await ctx.send(num_one * num_two)
 
-    embed = discord.Embed(
-        title=name + "Server Info",
-        description=description,
-        color=discord.Color.blue())
-    # embed.set_thumbnail(url=icon)
-    embed.add_field(name="Owner", value=owner, inline=True)
-    embed.add_field(name="Server ID", value=id, inline=True)
-    embed.add_field(name="Server Region", value=region, inline=True)
-    embed.add_field(
-        name="Member memberCount", value=member_count, inline=True)
-    await ctx.send(embed=embed)
+@bot.command()
+async def div(ctx, num_one: int, num_two: int):
+    await ctx.send(num_one / num_two)
 
 
-@client.event
-async def on_ready():
-    game = discord.Game('Killing Furrys')
-    await client.change_presence(status=discord.Status.idle, activity=game)
-    print('Loli online')
-
-
-@client.command()
-async def ban(ctx, member: discord.Member, *, reason=None):
-    await member.ban(reason=reason)
-    await ctx.send(f"Banned {member.mention}")
-
-
-@client.command()
-async def kick(ctx, member: discord.Member, *, reason=None):
-    await member.kick(reason=reason)
-    await ctx.send(f"Kicked {member.mention}")
-
-
-@client.command()
-async def warn(ctx, member: discord.Member, *, reason=None):
-    await member.send(f"You have been warned in {ctx.guild.name} for : {reason}")
-    await ctx.send(f"Warned {member.mention} for : {reason}")
-
-
-@client.event
-async def on_message(message):
-    if message.content.startswith('hello'):
-        await message.channel.send('Hello!')
-
-    if message.content.startswith('gay'):
-        await message.channel.send(':point_up_2: :rainbow_flag:')
-
-    if message.content.startswith('Loli'):
-        await message.channel.send("What's up?")
-
+#------------Events---------------------
+@bot.event 
+async def on_ready():  # When the bot is ready
+    print("Siempre te estoy viendo, Zero >.<")
+    print(bot.user)  # Yuno 5100    
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Zero Requiem"))
 
 extensions = [
-    'cogs.cog_example'
+	'cogs.cog_example' 
 ]
 
-if __name__ == '__main__':
-    for extension in extensions:
-        client.load_extension(extension)
+# ---------- Load Extensions ------------------------------
+if __name__ == '__main__':  # Ensures this is the file being ran
+	for extension in extensions:
+		bot.load_extension(extension)  # Loades every extension.
 
-keep_alive()
-token = os.environ.get("DISCORD_BOT_SECRET")
-client.run(token)
+# --------------- Bot Password------------------
+keep_alive()  # Starts a webserver to be pinged.
+token = os.environ.get("DISCORD_BOT_SECRET") 
+bot.run(token)  # Starts the bot
